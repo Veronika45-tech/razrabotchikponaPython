ca101033647bca92ef7f724f7b444b5cd3c6660d
Veronika45-tech <veronikaandreushkova@icloud.com>
2024-10-26 01:35:34 +0300

    Лабораторная работа 2

---
+++ Лабораторная работа 2/2 finance.py
@@ -0,0 +1,16 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+total_deficit = 0
+current_spend = spend
+
+for month in range(months):
+    if month >0:
+        current_spend *= (1 + increase)
+    monthly_deficit = current_spend - salary
+    total_deficit += monthly_deficit
+
+money_capital = round(total_deficit)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
