09842f19d57e09e5bdc991f0dfaa3b2368ad06f8
Veronika45-tech <veronikaandreushkova@icloud.com>
2024-11-01 01:14:03 +0300

    Update 1 finance.py

--- Лабораторная работа 2/1 finance.py
+++ Лабораторная работа 2/1 finance.py
@@ -3,6 +3,7 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 month = 0
+
 while money_capital >= 0:
     money_capital += salary
     money_capital -= spend
