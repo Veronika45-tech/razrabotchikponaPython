46c1d20c1928f6eb504bd798e750c161ef8572e7
Veronika45-tech <veronikaandreushkova@icloud.com>
2024-11-08 00:06:24 +0300

    Update 1index.py

--- лаба3/1index.py
+++ лаба3/1index.py
@@ -1,13 +1,15 @@
 def find_index(items_list, find_item):
-   for index, now_item in enumerate(items_list):
-       if now_item == find_item:
-           return index
+    for index, now_item in enumerate(items_list):
+        if now_item == find_item:
+            return index
+    return -1
+
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
 for find_item in ['банан', 'груша', 'персик']:
     index_item = find_index(items_list, find_item)
-    if index_item is not None:
+    if index_item != -1:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
         print(f"Товар '{find_item}' не найден в списке.")
